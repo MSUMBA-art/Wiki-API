8af912d50afe3f571f836ddd35a9c45173f8a5a8
ALEXANDER <msumba83@gmail.com>
2022-05-06 22:04:36 +0200

    GET, POST & DELETE METHODS

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-
 //jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", {
@@ -26,18 +27,40 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function (req, res) {
+app.route("/articles")
+  .get(function (req, res) {
     Article.find(function (err, foundArticles) {
-        if (!err) {
-           res.send(foundArticles); 
-        } else {
-            res.send(err);
-        }
-       
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
     });
-});
+  })
 
+  .post(function (req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.connect,
+    });
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("Successfully added a new article");
+      } else {
+        res.send(err);
+      }
+    });
+  })
 
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("Successfully deleted all articles");
+      } else {
+        res.send(err);
+      }
+    });
+  });
 
 
 
